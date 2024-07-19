86937c2a7c3a87040a6d58b6ca462ac08b771c41
llenny18 <ramosalleneid01@gmail.com>
2024-07-20 01:39:38 +0800

    psuh

--- lib/components/history.dart
+++ lib/components/history.dart
@@ -42,7 +42,21 @@ class _HistoryPageState extends State<HistoryTab> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('History of Cage Records', style: TextStyle(color: Colors.black)),
+        title: const Center(
+  child: Padding(
+    padding: EdgeInsets.all(20.0),
+    child: Text(
+      'History of Cage Records',
+      style: TextStyle(
+        color: Color(0xff034c57),
+        fontFamily: 'Metropolis',
+        fontSize: 40,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  ),
+),
+toolbarHeight: 100,
         backgroundColor: const Color(0xffeaf4f7),
       ),
       backgroundColor: const Color(0xffeaf4f7),
@@ -67,12 +81,12 @@ class _HistoryPageState extends State<HistoryTab> {
               });
             },
             children: const <Widget>[
-              Text('All'),
-              Text('Cage 1'),
-              Text('Cage 2'),
-              Text('Cage 3'),
-              Text('Cage 4'),
-              Text('Cage 5'),
+              Text('All', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Metropolis')),
+              Text('Cage 1', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Metropolis')),
+              Text('Cage 2', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Metropolis')),
+              Text('Cage 3', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Metropolis')),
+              Text('Cage 4', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Metropolis')),
+              Text('Cage 5', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Metropolis')),
             ],
           ),
           Expanded(
@@ -149,7 +163,7 @@ class _HistoryPageState extends State<HistoryTab> {
       ),
       elevation: 4.0,
       margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-      color: const Color(0xffe0dcdc),
+      color: const Color(0xffd7f3fc),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -157,13 +171,13 @@ class _HistoryPageState extends State<HistoryTab> {
           children: <Widget>[
             Text(
               date,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Color(0xff034c57), fontFamily: 'Metropolis'),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             Row(
               children: <Widget>[
                 Icon(icon, color: iconColor, size: 40),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -171,15 +185,16 @@ class _HistoryPageState extends State<HistoryTab> {
                       Text(
                         title,
                         style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 21,
                           fontWeight: FontWeight.bold,
                           color: iconColor,
+                          fontFamily: 'Metropolis'
                         ),
                       ),
-                      SizedBox(height: 5),
-                      Text(cage, style: TextStyle(fontSize: 14, color: Colors.black)),
-                      SizedBox(height: 5),
-                      Text('Cage Status: $status', style: TextStyle(fontSize: 14, color: Colors.black)),
+                      const SizedBox(height: 5),
+                      Text(cage, style: const TextStyle(fontSize: 17, color: Colors.black, fontFamily: 'Metropolis')),
+                      const SizedBox(height: 5),
+                      Text('Cage Status: $status', style: const TextStyle(fontSize: 17, color: Colors.black, fontFamily: 'Metropolis')),
                     ],
                   ),
                 ),
